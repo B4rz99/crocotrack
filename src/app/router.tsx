3c94d5853cfc57d87006a7a522bf038a5bde4e48
obarbozaa <obarbozaa@gmail.com>
2026-03-11 21:03:20 -0500

    feat(farms): add farms list page with CRUD

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -3,6 +3,8 @@ import { ProtectedRoute } from "@/features/auth/components/ProtectedRoute";
 import { LoginPage } from "@/features/auth/pages/LoginPage";
 import { RegisterPage } from "@/features/auth/pages/RegisterPage";
 import { OnboardingPage } from "@/features/onboarding/pages/OnboardingPage";
+import { FarmDetailPage } from "@/features/farms/pages/FarmDetailPage";
+import { FarmsPage } from "@/features/farms/pages/FarmsPage";
 import { ROUTES } from "@/shared/constants/routes";
 import { AppLayout } from "./layouts/AppLayout";
 import { AuthLayout } from "./layouts/AuthLayout";
@@ -11,8 +13,6 @@ import { OnboardingLayout } from "./layouts/OnboardingLayout";
 /* ---------- Placeholder pages ---------- */
 const InvitePage = () => <div>Invite</div>;
 const DashboardPage = () => <div>Dashboard</div>;
-const FarmsPage = () => <div>Farms</div>;
-const FarmDetailPage = () => <div>Farm Detail</div>;
 const SettingsPage = () => <div>Settings</div>;
 const SettingsTeamPage = () => <div>Settings Team</div>;
 
